ed5b8fa3fe9aaa6eda85fa813ad3afc6fc0df615
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-10-08 12:16:08 -0300

    Update aula68_exercicio_calculadora.py

--- aula68_exercicio_calculadora.py
+++ aula68_exercicio_calculadora.py
@@ -4,20 +4,30 @@ while True:
     segundo_digito = input('Digite o segundo número ')
     operador = input("Digite um operador: '+' '-' '*' '/' : ")
 
-    digitos_validos = None
-
     '''if primeiro_digito.isdigit() and segundo_digito.isdigit():
         print('digitos ok')
     else:
         print('Um ou ambos os números não são digitos, tente novemante')
         continue'''
-
+    num_1 = 0
+    num_2 = 0
     try:
         num_1 = float(primeiro_digito)
         num_2 = float(segundo_digito)
-        digitos_validos = True
     except:
         print('Um ou ambos os números não são digitos, tente novemante')
+        continue
+    if operador in '+-*/':
+        if operador == '+':
+            print(f'{num_1} + {num_2} = {num_1+num_2:.2f}')
+        elif operador == '-':
+            print(f'{num_1} - {num_2} = {num_1-num_2:.2f}')
+        elif operador == '/':
+            print(f'{num_1} / {num_2} = {num_1/num_2:.2f}')
+        elif operador == '*':
+            print(f'{num_1} * {num_2} = {num_1*num_2:.2f}')
+    else:
+        print('O operador não é valido, tente novemante')
         continue
 
     sair = input("Digite 'S' para sair ").lower().startswith('s')
