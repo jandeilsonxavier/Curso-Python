83d969a369e5256844cefcb22265251870c02801
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-10-08 11:44:14 -0300

    Update aula68_exercicio_calculadora.py

--- aula68_exercicio_calculadora.py
+++ aula68_exercicio_calculadora.py
@@ -4,17 +4,21 @@ while True:
     segundo_digito = input('Digite o segundo número ')
     operador = input("Digite um operador: '+' '-' '*' '/' : ")
 
-    if primeiro_digito.isdigit() and segundo_digito.isdigit():
+    digitos_validos = None
+
+    '''if primeiro_digito.isdigit() and segundo_digito.isdigit():
         print('digitos ok')
     else:
         print('Um ou ambos os números não são digitos, tente novemante')
-        continue
+        continue'''
 
     try:
-        num_1 = 
-
+        num_1 = float(primeiro_digito)
+        num_2 = float(segundo_digito)
+        digitos_validos = True
     except:
-        ...
+        print('Um ou ambos os números não são digitos, tente novemante')
+        continue
 
     sair = input("Digite 'S' para sair ").lower().startswith('s')
     if sair is True:
