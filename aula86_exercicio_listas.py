1cc729a89a5d34778e3572539ca9c255cc46d2b2
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-10-23 17:10:52 -0300

    Update aula86_exercicio_listas.py

--- aula86_exercicio_listas.py
+++ aula86_exercicio_listas.py
@@ -1,4 +1,36 @@
-a = [1,2,3]
-b = [4,5,6]
-c = a + b
-print(c)
+import os
+import time
+compras = []
+entrada = ''
+item_excluir = ''
+while True:
+    entrada = input('Selecione uma opção: [i]-inserir [a]-apagar [m]-mostrar: ').lower()
+    os.system('cls')
+    if entrada == 'i':
+        compras.append(input('Digite o nome do item para adicionar na lista: ').lower())
+        print('Item adicionado a lista.')
+        time.sleep(2)
+        os.system('cls')
+        continue
+    elif entrada == 'a':
+        item_excluir = input('Digite o nome do item para apagar: ').lower()
+        os.system('cls')
+        for i in compras:
+            if i == item_excluir:
+                compras.pop(compras.index(i))
+                print('Item apagado da lista!')
+                time.sleep(2)
+                os.system('cls')
+                continue
+    elif entrada == 'm':
+        for i in compras:
+            print(i)
+        time.sleep(4)
+        os.system('cls')
+        continue    
+    else:
+        print('Digite um das três opções!')
+        time.sleep(2)
+        os.system('cls')
+        continue
+        
