03d12955cbb04f73465b2b745fa5ae66fd8d64e4
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-12-06 20:32:18 -0300

    Create aula133_lambda_list_sort_sorted.py

---
+++ aula133_lambda_list_sort_sorted.py
@@ -0,0 +1,13 @@
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
