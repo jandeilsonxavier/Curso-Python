b88152daf97d3af31853fce0fead6254afc25b5f
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-12-06 20:33:01 -0300

    Update aula133_lambda_list_sort_sorted.py

--- aula133_lambda_list_sort_sorted.py
+++ aula133_lambda_list_sort_sorted.py
@@ -11,3 +11,10 @@ def exibir(lista):
     for item in lista:
         print(item)
     print()
+
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
