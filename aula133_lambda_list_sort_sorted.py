62f87c49c3fbfdacc4d3cbde09ca02244cefab1f
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-12-06 22:37:50 -0300

    Update aula133_lambda_list_sort_sorted.py

--- aula133_lambda_list_sort_sorted.py
+++ aula133_lambda_list_sort_sorted.py
@@ -1,20 +1,33 @@
-lista = [
-    {'nome': 'Luiz', 'sobrenome': 'miranda'},
-    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
-    {'nome': 'Daniel', 'sobrenome': 'Silva'},
-    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
-    {'nome': 'Aline', 'sobrenome': 'Souza'},
-]
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
 
 
-def exibir(lista):
-    for item in lista:
-        print(item)
-    print()
+# def exibir(lista):
+#     for item in lista:
+#         print(item)
+#     print()
 
 
-l1 = sorted(lista, key=lambda item: item['nome'])
-l2 = sorted(lista, key=lambda item: item['sobrenome'])
+# l1 = sorted(lista, key=lambda item: item['nome'])
+# l2 = sorted(lista, key=lambda item: item['sobrenome'])
 
-exibir(l1)
-exibir(l2)
+# exibir(l1)
+# exibir(l2)
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+# def soma(x, y):
+#     return x + y
+
+
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
