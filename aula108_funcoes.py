1546cac35a5ae6ca7c0734620ea6996c79410768
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-12-12 15:43:33 -0300

    Update aula108_funcoes.py

--- aula108_funcoes.py
+++ aula108_funcoes.py
@@ -39,4 +39,21 @@ def fatorial(n):
         return 1 
     return n * fatorial(n-1)
 
-print(fatorial(4))
+print(fatorial(5))
+
+def contagem(n):
+    if n == 1:
+        return 1 
+    return n * contagem(n-1)
+
+print(contagem(5))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# Exemplo de uso
+resultado = fibonacci(9)
+print(resultado)
