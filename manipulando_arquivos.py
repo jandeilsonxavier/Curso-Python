6a2b8b017a9468d437f43be9ab6a667fd214198d
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-12-27 13:11:33 -0300

    arquivos

---
+++ manipulando_arquivos.py
@@ -0,0 +1,6 @@
+arquivo = open('arquivo_teste.txt', 'r')
+# arquivo.write('1 linha do arquivo\n')
+# arquivo.write('2 linha do arquivo\n' '3 linha do arquivo')
+
+print(arquivo.readline())
+print(arquivo.readline())
